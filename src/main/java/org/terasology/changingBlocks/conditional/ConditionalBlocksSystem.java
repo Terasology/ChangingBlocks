2451c4aa455890bd74213af03d8e3b83c64b77ef
jdrueckert <jd.rueckert@googlemail.com>
2020-12-13 00:28:11 +0100

    chore: remove unused import

--- src/main/java/org/terasology/changingBlocks/conditional/ConditionalBlocksSystem.java
+++ src/main/java/org/terasology/changingBlocks/conditional/ConditionalBlocksSystem.java
@@ -15,7 +15,6 @@
  */
 package org.terasology.changingBlocks.conditional;
 
-import org.joml.RoundingMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.changingBlocks.conditional.components.ChangeBlockBlockDirectedComponent;
