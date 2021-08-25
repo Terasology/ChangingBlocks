5bbbc6ec701e3d29b7586af4f2afad1535c7a9f8
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 14:56:26 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#12)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/changingBlocks/conditional/components/ChangeBlockEntityNearbyComponent.java
+++ src/main/java/org/terasology/changingBlocks/conditional/components/ChangeBlockEntityNearbyComponent.java
@@ -1,24 +1,17 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.changingBlocks.conditional.components;
 
+import com.google.common.collect.Lists;
 import org.terasology.changingBlocks.conditional.BlockCondition;
 
 import java.util.List;
 
-public class ChangeBlockEntityNearbyComponent implements ConditionalBlockChangeComponent {
+public class ChangeBlockEntityNearbyComponent implements ConditionalBlockChangeComponent<ChangeBlockEntityNearbyComponent> {
     public List<BlockCondition.EntityNearby> changes;
+
+    @Override
+    public void copyFrom(ChangeBlockEntityNearbyComponent other) {
+        this.changes = Lists.newArrayList(other.changes);
+    }
 }
